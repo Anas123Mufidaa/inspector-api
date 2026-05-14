061988f3c4701fba2f729814bbe88bd8a947199d
nasschilik770 <nasschilik770@gmail.com>
2026-05-14 19:23:53 +0700

    api docs

--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,23 @@ import dotenv from 'dotenv';
 import passport from '../security/passport.js';
 import router from '../routes/index.js';
 import errorMiddleware from '../middlewares/error.js';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename);
 
 dotenv.config();
 
 const createServer = () => {
   const app = express();
+
+  app.get('/', (_req, res) => {
+    const html = readFileSync(join(__dirname, '../../public/index.html'), 'utf-8');
+    res.setHeader('Content-Type', 'text/html');
+    res.send(html);
+  });
 
   app.use(cors({
     origin: process.env.FRONTEND_URL,
